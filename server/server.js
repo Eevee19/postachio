aa823df928957b22bd329bfcc838d42b2475c7cc
Nicole Ip <nicoleapril29@gmail.com>
2020-06-26 13:41:00 -0500

    Working on OAuth

--- server/server.js
+++ server/server.js
@@ -4,6 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const client_id = "ba52815cf9a41762d41f";
+const client_secret = "f91cc3eef185cf904920b4aa4c7deefc7345b954";
+
+app.get("/github", (req, res) => {
+  //redirect to github authorization link
+  const uri = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=http://localhost:3000/user/login/callback`;
+
+  res.redirect(uri);
+});
 app.get("/user/login/callback", (req, res) => {
   const { query } = req;
   const { code } = query;
@@ -16,7 +25,7 @@ app.get("/user/login/callback", (req, res) => {
   }
   console.log("code", code);
 
-  //MAKE A POST REQUEST
+  //make a post request
 });
 
 app.listen(PORT, () => {
