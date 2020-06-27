ed5bf09545eee284d0072dd81fd0283578e29a0a
Shahrukh <Shahrukhan8@gmail.com>
2020-06-26 23:44:00 -0400

    Added search bar and modal pop up posts

--- client/components/Create.jsx
+++ client/components/Create.jsx
@@ -4,8 +4,8 @@ import Modal from './Modal.jsx';
 const Create = () => {
     return (
         <div>
-            
+            <input id="question-title"/>
         </div>
-    )
+          )
 }
 export default Create;
